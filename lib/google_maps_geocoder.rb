3e4508f519dd2286b8ba21791ec069a28353f15b
Maxime Menant <menant.maxime@gmail.com>
2013-01-17 15:52:13 +0100

    Fix logger to use 'data' instead of the old 'address' param

--- lib/google_maps_geocoder.rb
+++ lib/google_maps_geocoder.rb
@@ -35,7 +35,7 @@ class GoogleMapsGeocoder
     end
 
     logger = Logger.new(STDERR)
-    logger.info('GoogleMapsGeocoder') { "Geocoded \"#{address}\" and Google returned...\n#{@json.inspect}" }
+    logger.info('GoogleMapsGeocoder') { "Geocoded \"#{data}\" and Google returned...\n#{@json.inspect}" }
 
     @city, @country_short_name, @country_long_name, @county, @formatted_address, @formatted_street_address, @lat, @lng, @postal_code, @state_long_name, @state_short_name = parse_city, parse_country_short_name, parse_country_long_name, parse_county, parse_formatted_address, parse_formatted_street_address, parse_lat, parse_lng, parse_postal_code, parse_state_long_name, parse_state_short_name
   end
