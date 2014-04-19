6cc495364708c889d5a823fd9fbe71cebd38915e
Roderick Monje <rod@foveacentral.com>
2014-04-19 09:32:48 -0400

    spec: replaced specify/should with it/expect

--- spec/lib/google_maps_geocoder_spec.rb
+++ spec/lib/google_maps_geocoder_spec.rb
@@ -19,44 +19,44 @@ describe GoogleMapsGeocoder do
 
   context 'with "837 Union Street Brooklyn NY"' do
     subject { @exact_match }
-    specify { should be_exact_match }
+    it { expect(subject).to be_exact_match }
 
     context 'address' do
-      specify { subject.formatted_street_address.should == '837 Union Street' }
-      specify { subject.city.should == 'Brooklyn' }
-      specify { subject.county.should =~ /Kings/ }
-      specify { subject.state_long_name.should == 'New York' }
-      specify { subject.state_short_name.should == 'NY' }
-      specify { subject.postal_code.should =~ /112[0-9]{2}/ }
-      specify { subject.country_short_name.should == 'US' }
-      specify { subject.country_long_name.should == 'United States' }
-      specify { subject.formatted_address.should =~ /837 Union Street, Brooklyn, NY 112[0-9]{2}, USA/ }
+      it { expect(subject.formatted_street_address).to eq '837 Union Street' }
+      it { expect(subject.city).to eq 'Brooklyn' }
+      it { expect(subject.county).to match /Kings/ }
+      it { expect(subject.state_long_name).to eq 'New York' }
+      it { expect(subject.state_short_name).to eq 'NY' }
+      it { expect(subject.postal_code).to match /112[0-9]{2}/ }
+      it { expect(subject.country_short_name).to eq 'US' }
+      it { expect(subject.country_long_name).to eq 'United States' }
+      it { expect(subject.formatted_address).to match /837 Union Street, Brooklyn, NY 112[0-9]{2}, USA/ }
     end
 
     context 'coordinates' do
-      specify { subject.lat.should be_within(0.005).of(40.6748151) }
-      specify { subject.lng.should be_within(0.005).of(-73.9760302) }
+      it { expect(subject.lat).to be_within(0.005).of(40.6748151) }
+      it { expect(subject.lng).to be_within(0.005).of(-73.9760302) }
     end
   end
 
   context 'with "1600 Pennsylvania Washington"' do
     subject { @partial_match }
-    specify { should be_partial_match }
+    it { should be_partial_match }
 
     context 'address' do
-      specify { subject.formatted_street_address.should == '1600 Pennsylvania Avenue Northwest' }
-      specify { subject.city.should == 'Washington' }
-      specify { subject.state_long_name.should == 'District of Columbia' }
-      specify { subject.state_short_name.should == 'DC' }
-      specify { subject.postal_code.should =~ /2050[0-9]/ }
-      specify { subject.country_short_name.should == 'US' }
-      specify { subject.country_long_name.should == 'United States' }
-      specify { subject.formatted_address.should =~ /1600 Pennsylvania Avenue Northwest, President's Park, Washington, DC 2050[0-9], USA/ }
+      it { expect(subject.formatted_street_address).to eq '1600 Pennsylvania Avenue Northwest' }
+      it { expect(subject.city).to eq 'Washington' }
+      it { expect(subject.state_long_name).to eq 'District of Columbia' }
+      it { expect(subject.state_short_name).to eq 'DC' }
+      it { expect(subject.postal_code).to match /2050[0-9]/ }
+      it { expect(subject.country_short_name).to eq 'US' }
+      it { expect(subject.country_long_name).to eq 'United States' }
+      it { expect(subject.formatted_address).to match(/1600 Pennsylvania Avenue Northwest, President's Park, Washington, DC 2050[0-9], USA/) }
    end
 
    context 'coordinates' do
-      specify { subject.lat.should be_within(0.005).of(38.8976964) }
-      specify { subject.lng.should be_within(0.005).of(-77.0365191) }
+      it { expect(subject.lat).to be_within(0.005).of(38.8976964) }
+      it { expect(subject.lng).to be_within(0.005).of(-77.0365191) }
    end
   end
 end
