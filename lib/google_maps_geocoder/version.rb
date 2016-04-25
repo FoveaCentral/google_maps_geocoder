8a3afa753b6a788396887fdc47c05821dc4563f6
Roderick Monje <rod.monje@spreemo.com>
2016-04-25 16:22:07 -0400

    sys: bumped to 0.5.0

--- lib/google_maps_geocoder/version.rb
+++ lib/google_maps_geocoder/version.rb
@@ -1,4 +1,4 @@
 # A simple PORO wrapper for geocoding with Google Maps.
 class GoogleMapsGeocoder
-  VERSION = '0.4.0.pre1'.freeze unless defined?(GoogleMapsGeocoder::VERSION)
+  VERSION = '0.5.0'.freeze unless defined?(GoogleMapsGeocoder::VERSION)
 end
