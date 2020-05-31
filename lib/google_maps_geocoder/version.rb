4b5def920a9bfc21a778b7996abac1584a0af754
Roderick Monje <rod@foveacentral.com>
2020-05-30 23:23:19 -0400

    sys: bump to 0.7.2

--- lib/google_maps_geocoder/version.rb
+++ lib/google_maps_geocoder/version.rb
@@ -1,4 +1,4 @@
 # A simple PORO wrapper for geocoding with Google Maps.
 class GoogleMapsGeocoder
-  VERSION = '0.7.1'.freeze unless defined?(GoogleMapsGeocoder::VERSION)
+  VERSION = '0.7.2'.freeze unless defined?(GoogleMapsGeocoder::VERSION)
 end
