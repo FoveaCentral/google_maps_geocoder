1aaf044359d269ec7d0b454388464e093a7305c7
Roderick Monje <rod@foveacentral.com>
2021-11-21 23:56:26 -0800

    Revert "refactor"
    This reverts commit 1002b4d356d0c275a2f1abcff58da43230634b07.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,10 +15,15 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'google_maps_geocoder/google_maps_geocoder'
 # silence output
 RSpec.configure do |config|
+  original_stderr = $stderr
+  original_stdout = $stdout
   config.before(:context) do
-    allow_any_instance_of(Logger).to receive(anything).and_return true
+    # Redirect stderr and stdout
+    $stderr = File.open(File::NULL, 'w')
+    $stdout = File.open(File::NULL, 'w')
   end
   config.after(:context) do
-    allow_any_instance_of(Logger).to receive.and_call_original
+    $stderr = original_stderr
+    $stdout = original_stdout
   end
 end
